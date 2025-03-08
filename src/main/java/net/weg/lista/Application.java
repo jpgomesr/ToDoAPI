7a7119bba8c2bdc82dde605364dd89099ab090cc
João Paulo Gomes Rodrigues <joao_pg_rodrigues@estudante.sesisenai.org.br>
2025-03-07 21:51:45 -0300

    feat: Bd criado sozinho

--- src/main/java/net/weg/lista/Application.java
+++ src/main/java/net/weg/lista/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ListaDeTarefasEmSpringBootApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ListaDeTarefasEmSpringBootApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
